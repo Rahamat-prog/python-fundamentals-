3944c4778276c3548d902ec9962caf1a046f54dc
Rahamat-prog <rahamatshaikh216@gmail.com>
2026-07-27 23:54:42 +0530

    learn string and list data types

--- session2.py
+++ session2.py
@@ -3,13 +3,31 @@
 
 a = 5
 b = 5.0
-c = 2 + 4j
+c = 2 + 4j # j is a imaginary value which is 
 
 # print(type(a))
 # print(type(b))
 # print(type(c))
 
 
-d = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
-print(d[1])    
-print(d.get(2))
+# d = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+# print(d[1])    
+# print(d.get(2))
+
+# ...................1. String..............
+s = 'Welcome to the Geeks World'
+print(s)
+print(type(s))
+
+# access string with index
+# print(s[1])
+# print(s[-1])
+
+
+# ..........2. List...............
+a = [1, 2, 3]
+print(a)
+
+b = ["Geeks", "For", "Geeks", 4, 5]
+print(b[3])
+print(b[-3])
